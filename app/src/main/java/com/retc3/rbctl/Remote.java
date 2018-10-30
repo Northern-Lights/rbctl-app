eb6dedc580d456716db2447abc1fd99aba8250d5
Armen Boursalian <aboursalian@aol.com>
2018-10-30 08:02:40 -0700

    app builds w/ gRPC + protos

--- app/src/main/java/com/retc3/rbctl/Remote.java
+++ app/src/main/java/com/retc3/rbctl/Remote.java
@@ -3,6 +3,9 @@ package com.retc3.rbctl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.retc3.rbctl.rbctlsvc.Rbctl;
+import com.retc3.rbctl.rbctlsvc.RBCtlGrpc;
+
 public class Remote extends AppCompatActivity {
 
     @Override
