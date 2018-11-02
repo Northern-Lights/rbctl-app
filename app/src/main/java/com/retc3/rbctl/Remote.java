f58af3e9f66b681bf39eeac1dc842ea979318f8a
Armen Boursalian <aboursalian@aol.com>
2018-11-02 08:59:24 -0700

    Use text input to choose host & port

--- app/src/main/java/com/retc3/rbctl/Remote.java
+++ app/src/main/java/com/retc3/rbctl/Remote.java
@@ -3,6 +3,7 @@ package com.retc3.rbctl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.retc3.rbctl.rbctlsvc.Rbctl;
@@ -21,12 +22,45 @@ public class Remote extends AppCompatActivity {
     static RBCtlGrpc.RBCtlBlockingStub blockingStub;
     Logger logger = Logger.getLogger("remote");
 
+    private void setClient() {
+        if (Remote.channel != null) {
+            logger.info("Shutting down current connection");
+            try {
+                Remote.channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                logger.log(Level.WARNING, "Couldn't shutdown current connection", e);
+                Remote.channel.shutdownNow();
+            }
+        }
+
+        EditText hostInput = findViewById(R.id.input_host);
+        EditText portInput = findViewById(R.id.input_port);
+        String h = hostInput.getText().toString();
+        String s_port = portInput.getText().toString();
+        int p;
+        try {
+            p = Integer.parseInt(s_port);
+        } catch (NumberFormatException e) {
+            logger.log(Level.SEVERE, "Couldn't parse port input", e);
+            return;
+        }
+
+        Remote.channel = ManagedChannelBuilder.forAddress(h, p)
+                .usePlaintext(true)
+                .build();
+        Remote.blockingStub = RBCtlGrpc.newBlockingStub(Remote.channel);
+        logger.info("Host and port set");
+    }
+
     private void sendCommand(Rbctl.Command.Type t) {
         try {
             Rbctl.Command cmd = Rbctl.Command.newBuilder()
                     .setType(t)
                     .build();
             Rbctl.ControlResponse resp = Remote.blockingStub.control(cmd);
+        } catch (NullPointerException e) {
+            logger.log(Level.WARNING, "Channel or stub are nil; setting...");
+            setClient();
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error sending command", e);
         }
@@ -48,13 +82,6 @@ public class Remote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
-
-        logger.info("Creating channels and stuff");
-        Remote.channel = ManagedChannelBuilder.forAddress("192.168.1.1", 12123)
-                .usePlaintext(true)
-                .build();
-        Remote.blockingStub = RBCtlGrpc.newBlockingStub(Remote.channel);
-        logger.info(String.format("Are they null: %b %b", Remote.channel == null, Remote.blockingStub == null));
     }
 
     @Override
